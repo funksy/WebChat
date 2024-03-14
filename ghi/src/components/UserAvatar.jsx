16148625987d8c71a45cd877ef08cb6d8a33ab33
John Lukich <john.p.lukich@gmail.com>
2024-03-14 14:25:47 -0600

    fixed centering of logged in username

--- ghi/src/components/UserAvatar.jsx
+++ ghi/src/components/UserAvatar.jsx
@@ -1,11 +1,15 @@
 import { useGetTokenQuery } from '../store/apiSlice'
 
-const UserAvatar = () => { 
+const UserAvatar = () => {
     const { data } = useGetTokenQuery()
 
     return (
         <div className="flex flex-0 flex-col">
-            <div className="text-white w-36 self-center mt-4 truncate font-bold">{data?.account.username || 'loading'}</div>
+            <div className="text-white w-full mt-4  font-bold">
+                <p className="text-center truncate">
+                    {data?.account.username || 'loading'}
+                </p>
+            </div>
             <img
                 className="w-36 opacity-85 h-auto bg-custom-gold self-center m-4 mt-0 rounded-md shadow-[0_2px_4px_0] shadow-black"
                 src="../assets/robot-message-icon.svg"
