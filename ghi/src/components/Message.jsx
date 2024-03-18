0d1c13e683494121125406b3e53ace97d6638c74
Gary <gary.burns.tx@gmail.com>
2024-03-18 13:45:19 -0500

    RGB background generator for user icons in messages

--- ghi/src/components/Message.jsx
+++ ghi/src/components/Message.jsx
@@ -1,8 +1,20 @@
 import { useGetTokenQuery } from '../store/apiSlice'
 
+const getColorFromId = (str) => {
+    let sum = 0
+    for (let i = 0; i < str.length; i++) {
+        sum += str.charCodeAt(i)
+    }
+    const r = sum % 256
+    const g = (sum * 10) % 256
+    const b = (sum * 59) % 256
+    return `rgb(${r}, ${g}, ${b})`
+}
+
 const Message = (props) => {
     const { data } = useGetTokenQuery()
     const isCurrentUser = props.message.user_id === data.account.username
+    const color = getColorFromId(props.message.user_id)
 
     if (!props.isFirst) {
         return (
@@ -20,11 +32,11 @@ const Message = (props) => {
             <>
                 <div
                     className={`mt-4 font-bold flex text-lg text-custom-db items-center ${isCurrentUser
-                            ? 'justify-self-end flex-row-reverse' 
+                            ? 'justify-self-end flex-row-reverse'
                             : 'justify-self-start'
                         }`}
                 >
-                    <img className="inline bg-slate-200 rounded-2xl shadow-[0_2px_4px_0] shadow-black" src="../../assets/robot-message-icon.svg" width="35px" />
+                    <img style={{backgroundColor: color}} className="inline rounded-2xl shadow-[0_2px_4px_0] shadow-black" src="../../assets/robot-message-icon.svg" width="35px" />
                     <span
                         className={`mx-2 font-bold text-lg text-custom-db truncate ${isCurrentUser
                                 ? 'justify-self-end ml-[35px]'
