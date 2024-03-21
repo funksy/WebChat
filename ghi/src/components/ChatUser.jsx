fc0b02c5eac87c5d4ee14e70128fef98f9d10b6b
Gary <gary.burns.tx@gmail.com>
2024-03-21 10:53:24 -0500

    Add animation.

--- ghi/src/components/ChatUser.jsx
+++ ghi/src/components/ChatUser.jsx
@@ -1,6 +1,6 @@
 const ChatUser = (props) => {
   return (
-        <div className="chat-user w-36 text-center border rounded m-2 p-1 bg-custom-mauve">
+        <div className="chat-user w-36 text-center border rounded m-2 p-1 bg-custom-mauve animate-userjoin">
             <li className="truncate">{props.user}</li>
         </div>
   )
--- ghi/tailwind.config.js
+++ ghi/tailwind.config.js
@@ -3,6 +3,34 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      animation: {
+        userjoin: 'userjoin .5s ease-in-out',
+      },
+      keyframes: {
+        userjoin: {
+          '0%, 100%': {
+            transform: 'rotate(0deg)',
+          },
+          '12.5%': {
+            transform: 'rotate(-2.5deg)',
+          },
+          '25%': {
+            transform: 'rotate(0deg)',
+          },
+          '50%': {
+            transform: 'rotate(2.5deg)',
+          },
+          '62.5%': {
+            transform: 'rotate(0deg)',
+          },
+          '75%': {
+            transform: 'rotate(-2.5deg)',
+          },
+          '87.5%': {
+            transform: 'rotate(0deg)',
+          },
+        },
+      },
       fontFamily: {
         pop: ['Share Tech Mono']
       },
